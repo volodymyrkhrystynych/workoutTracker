7251fca7c7f60583f2b3227374e32755272cc641
Volodymyr Khrystynych <v.khryst@outlook.com>
2024-07-04 14:55:42 -0400

    more progress

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -12,7 +12,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const currentPage = Number(searchParams.get('page')) || 1;
   const createPageURL = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
-    params.set('page', '1');
+    params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`;
   };
   const allPages = generatePagination(currentPage, totalPages);
